10bca20e0fc5579b40f08e0b0604ffbe6840d474
Abi Liu <abiliu018@gmail.com>
2023-09-18 19:56:32 -0700

    added a repl cli interface to run language

--- src/main.js
+++ src/main.js
@@ -1,19 +1,21 @@
+const repl = require("repl");
 const Parser = require("./parser");
 const evaluate = require("./Runtime/evaluator");
 const Environment = require("./Runtime/environment");
 
 const env = new Environment(null);
-env.declareVar("x", { value: 100, type: "number" });
 // creating true false and null values inside the global runtime
 env.declareVar("true", { value: true, type: "boolean" }, true);
 env.declareVar("false", { value: false, type: "boolean" }, true);
 env.declareVar("null", { value: null, type: "null" }, true);
-const input = "const f = 5 f + 3";
 
 const parser = new Parser();
 
-let program = parser.produceAST(input);
-
-const result = evaluate(program, env);
-
-console.log(result);
+repl.start({
+  prompt: ">",
+  eval: function (input) {
+    let program = parser.produceAST(input);
+    const result = evaluate(program, env);
+    console.log(result);
+  },
+});
